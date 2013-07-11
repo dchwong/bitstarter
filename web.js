ef5f0412fabca2e1a0be8a4155384be04f5efdf0
dennis <decochw@gmail.com>
2013-07-10 20:39:51 -0700

    modify web.js

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var buf = new Buffer(256);
+    fs.readFile("index.html", function(err, data){
+
+	if (err) throw err;
+	buf.write(data);
+})
+    response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
